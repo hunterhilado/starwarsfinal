2fe8ff086f8667682b539d80f06d5340fa2a303f
hunterhilado <hunterhilado@gmail.com>
2020-04-25 12:57:35 -0400

    upd

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
-app.listen(PORT, function() {
+var PORT = 3000;
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
